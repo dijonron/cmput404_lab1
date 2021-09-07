cf47a4b9b3ff3fbaaa5ed017e5d1261549e68f58
Dalton Ronan <dronan@ualberta.ca>
2021-09-07 17:05:36 -0600

    fix

--- lab1.py
+++ lab1.py
@@ -7,6 +7,6 @@ print('\nGoogle Status Code: ', google.status_code)
 
 rawURL = 'https://raw.githubusercontent.com/dijonron/cmput404_lab1/main/lab1.py'
 sourceCode = requests.get(rawURL)
-print('\n--------- Source Code From GitHub ---------')
+
 print(sourceCode.text)
-print('-------------------------------------------')
+
