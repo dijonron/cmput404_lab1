c3d91dfbccd5674a84e341c0070b7337d2144512
Dalton Ronan <dronan@ualberta.ca>
2021-09-07 17:04:11 -0600

    get raw source code from github

--- lab1.py
+++ lab1.py
@@ -3,6 +3,10 @@ import requests
 print('requests version: ', requests.__version__)
 
 google = requests.get("http://www.google.com")
+print('\nGoogle Status Code: ', google.status_code)
 
-print(google.status_code)
-print(google.text)
+rawURL = 'https://raw.githubusercontent.com/dijonron/cmput404_lab1/main/lab1.py'
+sourceCode = requests.get(rawURL)
+print('\n--------- Source Code From GitHub ---------')
+print(sourceCode.text)
+print('-------------------------------------------')
